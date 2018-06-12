b83fec87da63a2f81744c0afc808ddd5db4c1502
Matt <matthewmagee05@gmail.com>
2018-06-12 09:12:54 -0400

    deleted all the routing stuff, components no longer needed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,18 +4,12 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { FooterComponent } from './components/footer/footer.component';
-import { LoginComponent } from './components/login/login.component';
-import { LogoutComponent } from './components/logout/logout.component';
-import { MainComponent } from './components/main/main.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    FooterComponent,
-    LoginComponent,
-    LogoutComponent,
-    MainComponent
+    FooterComponent
   ],
   imports: [
     BrowserModule
